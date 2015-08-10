5f247f8f961af3a436828dc5453ccc513411136f
Fred Moolekamp <fred3public@gmail.com>
2015-08-10 19:58:56 -0400

    fixed __init__.py

--- astropyp/__init__.py
+++ astropyp/__init__.py
@@ -1,8 +1,15 @@
-import config
-import tasks
-import index
-import utils
-import pipeline
-import phot
-import catalog
-from pipeline import Pipeline
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This is an Astropy affiliated package.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    pass
