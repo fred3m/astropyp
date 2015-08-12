d10eac7121b1d4e1a4ae4614b66273c3e37359d5
Fred Moolekamp <fred3public@gmail.com>
2015-08-12 02:25:23 -0400

    fixed data files in test

--- astropyp/tests/setup_package.py
+++ astropyp/tests/setup_package.py
@@ -1,3 +1,18 @@
+import os
+
 def get_package_data():
     return {
-        _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc']}
+        _ASTROPY_PACKAGE_NAME_ + '.tests': [
+            'coveragerc',
+            os.path.join('data', 'fake_obs0.fits'),
+            os.path.join('data', 'fake_obs1.fits'),
+            os.path.join('data', 'fake_obs2.fits'),
+            os.path.join('data', 'fake_obs3.fits'),
+            os.path.join('data', 'fake_obs4.fits'),
+            os.path.join('data', 'fake_obs5.fits'),
+            os.path.join('data', 'fake_obs6.fits'),
+            os.path.join('data', 'fake_obs7.fits'),
+            os.path.join('data', 'fake_obs8.fits'),
+            os.path.join('data', 'fake_obs9.fits')
+        ],
+    }
